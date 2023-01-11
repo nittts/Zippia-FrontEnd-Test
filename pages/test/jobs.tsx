396795bbd435f7eb00d41b3789fd9092281d5c85
William Baierle <w.baierle@gmail.com>
2023-01-11 17:34:12 -0300

    fix: tab title

--- pages/test/jobs.tsx
+++ pages/test/jobs.tsx
@@ -8,6 +8,7 @@ import Card from "../../components/Card";
 import Filter from "../../components/Filter";
 import Header from "../../components/Header";
 import Loading from "../../components/Loading";
+import Head from "next/head";
 
 export default function Main() {
   const [data, setData] = useState<IRequestData>({} as IRequestData);
@@ -58,38 +59,49 @@ export default function Main() {
   }, [payload]);
 
   return (
-    <div className={styles.container}>
-      <Header />
-      <h1>DEVELOPER JOBS NEAR ME - {data.totalJobs} JOBS</h1>
-      <Filter sortFn={sortFn} />
-      <div className={styles.cardsContainer}>
-        {loading ? (
-          <Loading />
-        ) : (
-          <>
-            {
-              // Map through all the jobs listed on Jobs[], rendering the component card with their info
-              data.jobs &&
-                data.jobs.map((singleJob, index) => {
-                  // Destructure of the Job Object, searching for the requested Params plus extra info
-                  const { jobTitle, companyName, jobDescription, companyLogo, postedDate, estimatedSalary } = singleJob;
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="keywords" content="titla, meta, nextjs" />
+        <meta name="author" content="Syamlal CM" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Home - Zippia Test</title>
+      </Head>
 
-                  // Scope Declaring types to match prop typing
-                  const data: ICardData = {
-                    jobTitle: jobTitle as string,
-                    companyName: companyName as string,
-                    jobDesc: jobDescription as string,
-                    companyLogo: companyLogo as string,
-                    postedDate: postedDate as string,
-                    estimatedSalary: estimatedSalary as string,
-                  };
-                  return <Card data={data} key={`${index}`} />;
-                })
-            }
-          </>
-        )}
+      <div className={styles.container}>
+        <Header />
+        <h1>DEVELOPER JOBS NEAR ME - {data.totalJobs} JOBS</h1>
+        <Filter sortFn={sortFn} />
+        <div className={styles.cardsContainer}>
+          {loading ? (
+            <Loading />
+          ) : (
+            <>
+              {
+                // Map through all the jobs listed on Jobs[], rendering the component card with their info
+                data.jobs &&
+                  data.jobs.map((singleJob, index) => {
+                    // Destructure of the Job Object, searching for the requested Params plus extra info
+                    const { jobTitle, companyName, jobDescription, companyLogo, postedDate, estimatedSalary } =
+                      singleJob;
+
+                    // Scope Declaring types to match prop typing
+                    const data: ICardData = {
+                      jobTitle: jobTitle as string,
+                      companyName: companyName as string,
+                      jobDesc: jobDescription as string,
+                      companyLogo: companyLogo as string,
+                      postedDate: postedDate as string,
+                      estimatedSalary: estimatedSalary as string,
+                    };
+                    return <Card data={data} key={`${index}`} />;
+                  })
+              }
+            </>
+          )}
+        </div>
+        <button onClick={fetchData}>Load More</button>
       </div>
-      <button onClick={fetchData}>Load More</button>
-    </div>
+    </>
   );
 }
